18c81fd53f24433970fb13b9e8de6f9d62025f70
Artur Carrah <arturcarrah@alu.ufc.br>
2025-10-05 16:21:22 -0300

    Q1

---
+++ Homework 1/HW1Q1.R
@@ -0,0 +1,42 @@
+library(ggplot2)
+
+x <- c(15.8, 22.7, 26.8, 19.1, 18.5, 14.4, 8.3, 25.9, 26.4, 9.8, 21.9, 10.5, 17.3, 6.2, 18.0, 22.9, 24.6, 19.4, 12.3, 15.9, 20.1, 17.0, 22.3, 27.5, 23.9, 17.5, 11.0, 20.4, 16.2, 20.8, 20.9, 21.4, 18.0, 24.3, 11.8, 17.9, 18.7, 12.8, 15.5, 19.2, 13.9, 28.6, 19.4, 21.6, 13.5, 24.6, 20.0, 24.1, 9.0, 17.6, 25.7, 20.1, 13.2, 23.7, 10.7, 19.0, 14.5, 18.1, 31.8, 28.5, 22.7, 15.2, 23.0, 29.6, 11.2, 14.7, 20.5, 26.6, 13.3, 18.1, 24.8, 26.1, 7.7, 22.5, 19.3, 19.4, 16.7, 16.9, 23.5, 18.4)
+
+y = sort(x)
+y 
+
+#https://stackoverflow.com/questions/2547402/how-to-find-the-statistical-mode
+
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+median(x)
+Mode(x)
+mean = mean(x)
+mean
+amplitude = y[80] - y[1]
+amplitude
+var = var(x) # Variancia de amostra (dividido por n-1)
+var
+std = sd(x) # Desvio padrão de amostra (dividido por n-1)
+std
+
+coef_var = var/mean # Coeficiente de variação = variância/média
+coef_var
+
+df <- as.data.frame(x)
+
+hist <- ggplot(df, aes(x=x)) + 
+  geom_histogram(binwidth=1, color="black", fill="white")
+
+hist
+
+boxplot <- ggplot(df, aes(x=x))+
+            geom_boxplot(fill = "dark green")
+
+boxplot
+
+quantils <-quantile(y, c(0.25,0.5,0.75), type = 2) # Tipo 2 tira a média
+quantils
