281a80bdb8494b3b4e8c76abec19c039bc5d149e
Artur Carrah <arturcarrah@alu.ufc.br>
2025-10-09 08:26:12 -0300

    Comentários

--- Homework 1/HW1Q2.R
+++ Homework 1/HW1Q2.R
@@ -1,4 +1,9 @@
-library(ggplot2)
+library(ggplot2) # ggplot2 para fazer os gráficos
+
+# Criando um dataframe com as colunas de idade, nacionalidade, renda e experiência (Obrigado ChatoGPT!)
+print("Tabela Original")
+cat("\n")
+
 df <- data.frame(
   idade = c(28, 34, 46, 26, 37, 29, 51, 31, 39, 43, 58, 44, 25, 23, 52, 42, 48, 33, 38, 46),
   nacionalidade = c("Italiana", "Inglesa", "Belga", "Espanhola", "Italiana", "Espanhola", 
@@ -10,37 +15,55 @@ df <- data.frame(
   experiencia = c(2, 8, 21, 1, 15, 3, 28, 5, 13, 20, 32, 23, 1, 0, 29, 18, 19, 7, 12, 23)
 )
 
-print("Idade")
+print(df)
+cat("\n") # O uso do cat é para criar uma nova linha e espaçar as informações
 
-media_idade = mean(df$idade)
+print("Idade")
+cat("\n")
+
+# Para pegar só uma coluna do dataframe coloque df$nome_coluna
+# Estatistícas de renda, idade e experiência
+
+media_idade = mean(df$idade) 
 mediana_idade = median(df$idade)
 std_idade = sd(df$idade)
-media_idade
-mediana_idade
-std_idade
+sprintf("Media de idade dos candidatos: %f", media_idade)
+sprintf("Mediana das idades dos candidatos: %f", mediana_idade)
+sprintf("Desvio padrao das idades dos candidatos: %f", std_idade)
+
+cat("\n")
 
 print("Renda")
+cat("\n")
 
 media_renda = mean(df$renda)
 mediana_renda = median(df$renda)
 std_renda = sd(df$renda)
-media_renda
-mediana_renda
-std_renda
+sprintf("Media da renda desejada pelos candidatos: %f", media_renda)
+sprintf("Mediana da renda desejada pelos candidatos: %f", mediana_renda)
+sprintf("Desvio padrao das rendas desejadas pelos candidatos: %f", std_renda)
+
+cat("\n")
 
 print("Experiencia")
+cat("\n")
 
 media_exp = mean(df$experiencia)
 mediana_exp = median(df$experiencia)
 std_exp = sd(df$experiencia)
-media_exp
-mediana_exp
-std_exp
+sprintf("Media de anos de experiencia dos candidatos: %f", media_exp)
+sprintf("Mediana dos anos de experiencia dos candidatos: %f", mediana_exp)
+sprintf("Desvio padrao dos anos de experiencia dos candidatos: %f", std_exp)
 
+cat("\n")
 
-print(df)
+# Agora mostrando os dataframes divididos por nacionalidade e fazendo os devidos cálculos
+# Use subset para criar outro dataframe a partir de um dado específico
 
-print("Dataframes divididos")
+# Copia e cola infinito abaixo para printar cada dataframe
+
+print("Dataframes divididos por nacionalidade: ")
+cat("\n")
 
 df_italiana <- subset(df, nacionalidade == "Italiana")
 df_inglesa <- subset(df, nacionalidade == "Inglesa")
@@ -49,52 +72,84 @@ df_espanhola <- subset(df, nacionalidade == "Espanhola")
 df_francesa <- subset(df, nacionalidade == "Francesa")
 df_alema <- subset(df, nacionalidade == "Alemana")
 
+print("Italianos")
+cat("\n")
+
 print(df_italiana)
 rendaMedia_ita <- mean(df_italiana$renda)
 expMedia_ita <- mean(df_italiana$experiencia)
-rendaMedia_ita
-expMedia_ita
+cat("\n")
+sprintf("Renda media italiana: %f", rendaMedia_ita)
+sprintf("Experiencia media italiana: %f", expMedia_ita)
+cat("\n")
+
+print("Ingleses")
+cat("\n")
 
 print(df_inglesa)
 rendaMedia_eng <- mean(df_inglesa$renda)
 expMedia_eng <- mean(df_inglesa$experiencia)
-rendaMedia_eng
-expMedia_eng
+cat("\n")
+sprintf("Renda media inglesa: %f", rendaMedia_eng)
+sprintf("Experiencia media inglesa: %f", expMedia_eng)
+cat("\n")
+
+print("Belgas")
+cat("\n")
 
 print(df_belga)
 rendaMedia_bel <- mean(df_belga$renda)
 expMedia_bel <- mean(df_belga$experiencia)
-rendaMedia_bel
-expMedia_bel
+cat("\n")
+sprintf("Renda media dos belgas: %f", rendaMedia_bel)
+sprintf("Experiencia media dos belgas: %f", expMedia_bel)
+cat("\n")
+
+print("Espanhois")
+cat("\n")
 
 print(df_espanhola)
 rendaMedia_esp <- mean(df_espanhola$renda)
 expMedia_esp <- mean(df_espanhola$experiencia)
-rendaMedia_esp
-expMedia_esp
+cat("\n")
+sprintf("Renda media dos espanhois: %f", rendaMedia_esp)
+sprintf("Experiencia media dos espanhois: %f", expMedia_esp)
+cat("\n")
+
+print("Franceses")
+cat("\n")
 
 print(df_francesa)
 rendaMedia_fra <- mean(df_francesa$renda)
 expMedia_fra <- mean(df_francesa$experiencia)
-rendaMedia_fra
-expMedia_fra
+cat("\n")
+sprintf("Renda media dos franceses: %f", rendaMedia_fra)
+sprintf("Experiencia media dos franceses: %f", expMedia_fra)
+cat("\n")
+
+print("Alemaes")
+cat("\n")
 
 print(df_alema)
 rendaMedia_ger <- mean(df_alema$renda)
 expMedia_ger <- mean(df_alema$experiencia)
-rendaMedia_ger
-expMedia_ger
+cat("\n")
+sprintf("Renda media dos alemaes: %f", rendaMedia_ger)
+sprintf("Experiencia media dos alemaes: %f", expMedia_ger)
+cat("\n")
 
 
-plot(df$experiencia, df$renda, xlab = "Anos de experiencia", 
+plot(df$experiencia, df$renda, xlab = "Anos de experiencia", # Plotando o gráfico de Renda x Experiência e traçando uma reta indicando
+                                                             # correlação entre os dados
     ylab = "Renda desejada", col = "blue", pch = 19)
 
 model <- lm(df$renda ~ df$experiencia, data = df)
 abline(model)
 
-correlacao <- cor(df$experiencia, df$renda, method = 'pearson')
-correlacao
+correlacao <- cor(df$experiencia, df$renda, method = 'pearson') # Calculando o coeficiente de correlação de Pearson.
+sprintf("Coeficiente de correlacao de Pearson: %f", correlacao) # Por ser próximo de 0.5 a correlação existe e é razoável
 
+# Plotando histogramas para cada nacionalidade
 
 idade_ita <- df_italiana$idade
 renda_ita <- df_italiana$renda
